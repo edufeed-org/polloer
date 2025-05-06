590e69c1f4484b85c7fb121cd390b2e95fee4a47
@s.roertgen <sroertgen@gmail.com>
2025-05-06 08:32:52 +0200

    fix login with Extension, formatting

--- src/lib/index.js
+++ src/lib/index.js
@@ -1,33 +1,35 @@
-import { NDKNip07Signer, NDKPrivateKeySigner } from "@nostr-dev-kit/ndk";
-import { ndk } from "$lib/stores";
-import { get } from "svelte/store";
+import { NDKNip07Signer, NDKPrivateKeySigner } from '@nostr-dev-kit/ndk';
+import { ndk } from '$lib/stores';
+import { get } from 'svelte/store';
 
 export async function login() {
-    if (window.nostr) {
-        const signer = new NDKNip07Signer();
-        get(ndk).signer = signer;
-        signer.user();
-    } else {
-        console.log("no extension")
-        const storedPrivateKey = window.localStorage.getItem('nostrPrivateKey');
-        if (storedPrivateKey) {
-            const privateKey = JSON.parse(storedPrivateKey);
-            console.log("stored private key", privateKey)
-            const signer = new NDKPrivateKeySigner(privateKey);
-            ndk.update((ndk) => {
-                ndk.signer = signer;
-                return ndk;
-            });
-            // get(ndk).signer = signer;
-            signer.user();
-        } else {
-            console.log('No private key found, generating a new one...');
-            const privateKey = NDKPrivateKeySigner.generate();
-            const signer = new NDKPrivateKeySigner(privateKey.privateKey);
-            console.log('Generated Private Key:', privateKey);
-            signer.user();
-            window.localStorage.setItem('nostrPrivateKey', JSON.stringify(privateKey.privateKey));
-            get(ndk).signer = signer;
-        }
-    }
+	if (window.nostr) {
+		const signer = new NDKNip07Signer();
+		ndk.update((ndk) => {
+			ndk.signer = signer;
+			return ndk;
+		});
+		signer.user();
+	} else {
+		console.log('no extension');
+		const storedPrivateKey = window.localStorage.getItem('nostrPrivateKey');
+		if (storedPrivateKey) {
+			const privateKey = JSON.parse(storedPrivateKey);
+			console.log('stored private key', privateKey);
+			const signer = new NDKPrivateKeySigner(privateKey);
+			ndk.update((ndk) => {
+				ndk.signer = signer;
+				return ndk;
+			});
+			signer.user();
+		} else {
+			console.log('No private key found, generating a new one...');
+			const privateKey = NDKPrivateKeySigner.generate();
+			const signer = new NDKPrivateKeySigner(privateKey.privateKey);
+			console.log('Generated Private Key:', privateKey);
+			signer.user();
+			window.localStorage.setItem('nostrPrivateKey', JSON.stringify(privateKey.privateKey));
+			get(ndk).signer = signer;
+		}
+	}
 }
