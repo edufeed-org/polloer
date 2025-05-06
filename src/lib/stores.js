48d393d9bbc27fea5f30c49f5bd9d333a2b126ff
@s.roertgen <sroertgen@gmail.com>
2025-05-06 09:09:08 +0200

    format

--- src/lib/stores.js
+++ src/lib/stores.js
@@ -1,21 +1,19 @@
-import NDKSvelte from "@nostr-dev-kit/ndk-svelte";
-import { writable, derived } from "svelte/store";
-import { NDKUser } from "@nostr-dev-kit/ndk";
+import NDKSvelte from '@nostr-dev-kit/ndk-svelte';
+import { writable, derived } from 'svelte/store';
+import { NDKUser } from '@nostr-dev-kit/ndk';
 
 export let connected = writable(false);
 
 const _ndk = new NDKSvelte({
-  explicitRelayUrls: [
-    'wss://relay-rpi.edufeed.org'
-  ],
+	explicitRelayUrls: ['wss://relay-rpi.edufeed.org']
 });
 
-export const ndk = writable(_ndk)
+export const ndk = writable(_ndk);
 
-export const user = derived(ndk, $ndk => {
-  console.log("updating user")
-  if ($ndk.signer !== undefined) {
-    return new NDKUser($ndk.signer);
-  }
-  return undefined;
+export const user = derived(ndk, ($ndk) => {
+	console.log('updating user');
+	if ($ndk.signer !== undefined) {
+		return new NDKUser($ndk.signer);
+	}
+	return undefined;
 });
